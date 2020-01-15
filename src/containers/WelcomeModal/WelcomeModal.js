24d66fc3d92831af29ad530b24e7aaa96318f60f
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2020-01-15 09:17:18 -0700

    Add an error message to appear in the form before creating a user and log user onto the next page, prevented user from moving forward without filling out the form.

--- src/containers/WelcomeModal/WelcomeModal.js
+++ src/containers/WelcomeModal/WelcomeModal.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { createUser, hasErrored } from '../../actions'
+import { createUser, hasErrored } from '../../actions';
 import { startConversation } from '../../apiCalls';
-import './WelcomeModal.css'
+import './WelcomeModal.css';
 
 export class WelcomeModal extends Component {
   constructor() {
@@ -12,70 +12,78 @@ export class WelcomeModal extends Component {
       firstName: '',
       lastName: '',
       feeling: '',
-      error: '',
-    }
+      error: ''
+    };
   }
 
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value, error: '' });
-  }
+  };
 
   handleSubmit = e => {
     const { firstName, lastName, feeling } = this.state;
     e.preventDefault();
-    this.props.createUser({
-      id: Date.now(),
-      firstName,
-      lastName,
-      feeling,
-    });
-    this.connectToChatBot();
-  }
+    if (firstName === '' || lastName === '' || feeling === '') {
+      this.setState({
+        error:
+          'Please make sure to have First Name, Last Name, and Feeling filled out'
+      });
+    } else {
+      this.props.createUser({
+        id: Date.now(),
+        firstName,
+        lastName,
+        feeling
+      });
+      this.connectToChatBot();
+    }
+  };
 
   connectToChatBot = async () => {
     try {
       const firstMessage = await startConversation(this.state.feeling);
       this.props.addMessage(firstMessage.message, false);
-    } catch({ message }) {
+    } catch ({ message }) {
       this.props.hasErrored(message);
     }
-  }
+  };
 
   render() {
     const { firstName, lastName, feeling, error } = this.state;
     return (
-      <form className="welcome-modal">
-        <legend>Welcome to Survey Bot!  Please enter your name.</legend>
-        {error && <p className="error-msg">{error}</p>}
-        <label>First Name:
+      <form className='welcome-modal'>
+        <legend>Welcome to Survey Bot! Please enter your name.</legend>
+        {error && <p className='error-msg'>{error}</p>}
+        <label>
+          First Name:
           <input
-            name="firstName"
+            name='firstName'
             value={firstName}
             onChange={this.handleChange}
           />
         </label>
-        <label>Last Name:
-        <input
-            name="lastName"
+        <label>
+          Last Name:
+          <input
+            name='lastName'
             value={lastName}
             onChange={this.handleChange}
           />
         </label>
-        <select name="feeling" value={feeling} onChange={this.handleChange}>
-          <option value="">How are you feeling currently?</option>
-          <option value="happy">Happy</option>
-          <option value="tired">Tired</option>
-          <option value="stressed">Stressed</option>
-          <option value="frustrated">Frustrated</option>
+        <select name='feeling' value={feeling} onChange={this.handleChange}>
+          <option value=''>How are you feeling currently?</option>
+          <option value='happy'>Happy</option>
+          <option value='tired'>Tired</option>
+          <option value='stressed'>Stressed</option>
+          <option value='frustrated'>Frustrated</option>
         </select>
-        <button onClick={this.handleSubmit}>
-          Take 5 minutes to check in!
-        </button>
+        <button onClick={this.handleSubmit}>Take 5 minutes to check in!</button>
       </form>
-    )
+    );
   }
 }
 
-export const mapDispatchToProps = dispatch => bindActionCreators({ createUser, hasErrored }, dispatch)
+export const mapDispatchToProps = dispatch =>
+  bindActionCreators({ createUser, hasErrored }, dispatch);
 
 export default connect(null, mapDispatchToProps)(WelcomeModal);
